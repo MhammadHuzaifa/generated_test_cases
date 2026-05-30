9d683b7786fd5e54e72bbb9cc2c0c0929e950c72
Muhammad Huzaifa <huzaaifa0323@gmail.com>
2026-05-30 15:05:07 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# config_vulnerable.py
+AWS_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE"
+API_TOKEN = "1234567890abcdef"
+DATA_PATH = "../../../etc/passwd"
+MODEL_PATH = "/tmp/unsafe_model.pkl"
+RANDOM_STATE = 42
+TEST_SIZE = 0.2
